54db8444113d4ed94c8c08c89b98cf62dad39803
Stefan Cordes <mail@quaddy-services.de>
2020-01-22 21:50:04 +0100

    format

--- collect_values.py
+++ collect_values.py
@@ -14,7 +14,6 @@ from homematicip.rule import *
 from homematicip.home import Home
 from homematicip.base.helpers import handle_config
 
-
 logger = None
 
 
@@ -46,40 +45,39 @@ def fake_download_configuration():
 
 def main():
 
-	_config = homematicip.find_and_load_config_file()
-	if _config is None:
-		print("Could not find configuration file. Script will exit")
-		return
+    _config = homematicip.find_and_load_config_file()
+    if _config is None:
+        print("Could not find configuration file. Script will exit")
+        return
 
-	global logger
-	logger = create_logger(
-		_config.log_level, _config.log_file
-	)
+    global logger
+    logger = create_logger(
+        _config.log_level, _config.log_file
+    )
 
-	home = Home()
-	home.set_auth_token(_config.auth_token)
-	home.init(_config.access_point)
+    home = Home()
+    home.set_auth_token(_config.auth_token)
+    home.init(_config.access_point)
 
-	if not home.get_current_state():
-		print("home.get_current_state()={}".format(home.get_current_state()))
-		return
+    if not home.get_current_state():
+        print("home.get_current_state()={}".format(home.get_current_state()))
+        return
 
-	sortedDevices = sorted(home.devices, key=attrgetter("deviceType", "label"))
-	# print("sortedDevices={}".format(sortedDevices))
-	for d in sortedDevices:
-		if isinstance(d, TemperatureHumiditySensorWithoutDisplay):
-		  print("humidity {},{},{}".format(d.label,d.actualTemperature,d.humidity))
-		  f=open(d.label+".csv", "a+")
-		  f.write("{},{},{}\r\n".format(datetime.now(),d.actualTemperature,d.humidity))
-		  f.close()
-		elif isinstance(d, HeatingThermostat):
- 			print("valvePosition {},{},{}".format(d.label,d.valveActualTemperature,d.valvePosition))
- 			f=open(d.label+".csv", "a+")
- 			f.write("{},{},{}\r\n".format(datetime.now(),d.valveActualTemperature,d.valvePosition))
- 			f.close()
-		else:
-			print("{} {}".format(d.id, str(d)))
-
+    sortedDevices = sorted(home.devices, key=attrgetter("deviceType", "label"))
+    # print("sortedDevices={}".format(sortedDevices))
+    for d in sortedDevices:
+        if isinstance(d, TemperatureHumiditySensorWithoutDisplay):
+            print("humidity {},{},{}".format(d.label, d.actualTemperature, d.humidity))
+            f = open(d.label + ".csv", "a+")
+            f.write("{},{},{}\r\n".format(datetime.now(), d.actualTemperature, d.humidity))
+            f.close()
+        elif isinstance(d, HeatingThermostat):
+            print("valvePosition {},{},{}".format(d.label, d.valveActualTemperature, d.valvePosition))
+            f = open(d.label + ".csv", "a+")
+            f.write("{},{},{}\r\n".format(datetime.now(), d.valveActualTemperature, d.valvePosition))
+            f.close()
+        else:
+            print("{} {}".format(d.id, str(d)))
 
 
 def printEvents(eventList):
